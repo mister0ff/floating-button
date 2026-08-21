55419ef8dfcab5db42b3e70ac7a782004d26e78f
Eduhh <frazaoe620@gmail.com>
2026-08-21 19:48:41 -0300

    Update Android and Kotlin plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-
